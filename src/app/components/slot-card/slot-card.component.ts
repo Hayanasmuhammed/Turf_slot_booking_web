9ceab73d69789e7b7530c85b633812749408a24a
Hayanasmuhammed <hayanasck493@gmail.com>
2025-07-13 13:54:08 +0530

    initial commit

---
+++ src/app/components/slot-card/slot-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slot-card',
+  imports: [],
+  templateUrl: './slot-card.component.html',
+  styleUrl: './slot-card.component.scss'
+})
+export class SlotCardComponent {
+
+}
