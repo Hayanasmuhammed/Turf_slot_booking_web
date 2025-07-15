09457cbf733bc9e8207840da8aae33c2f72fd127
Hayanasmuhammed <hayanasck493@gmail.com>
2025-07-15 18:02:21 +0530

    new login page is added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule],
+  imports: [RouterModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+
+  isLogin = true;
+  authForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  switchMode(isLogin: boolean): void {
+    this.isLogin = isLogin;
+    this.authForm.reset();
+  }
+
+  initForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.authForm.valid) {
+      const formData = this.authForm.value;
+      if (this.isLogin) {
+        console.log('Login', formData);
+      } else {
+        console.log('Signup', formData);
+      }
+    }
+  }
 
 }
